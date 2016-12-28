d48fdfe973adbcd44d0edc0713ad7ca8612ee8c0
Johannes Brüderl <johannes.bruederl@gmail.com>
2016-12-28 01:01:19 +0100

    initial import

---
+++ FuBar_ElysiumPopulationFu.lua
@@ -0,0 +1,51 @@
+FuBar_ElysiumPopulationFu = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0", "AceConsole-2.0", "AceDB-2.0", "FuBarPlugin-2.0")
+FuBar_ElysiumPopulationFu.hasIcon = true
+FuBar_ElysiumPopulationFu.hasNoColor = true
+FuBar_ElysiumPopulationFu.clickableTooltip = true
+FuBar_ElysiumPopulationFu.folderName = "FuBar_ElysiumPopulationFu"
+
+local waitForWho = 0
+
+function ElysiumPopulationFu_ChatFrame_OnEvent(event)
+  -- Ensure that we don't accidently omit other chat messages
+  if waitForWho == 1 and event == "CHAT_MSG_SYSTEM" and arg1 == "0 players total" and arg2 == "" then
+    waitForWho = 0
+     else
+       OrigChatFrame_OnEvent(event);
+  end
+end
+
+function FuBar_ElysiumPopulationFu:OnInitialize()
+  OrigChatFrame_OnEvent = ChatFrame_OnEvent;
+  ChatFrame_OnEvent = ElysiumPopulationFu_ChatFrame_OnEvent;
+
+  self:SetText("Updating...")
+end
+
+function FuBar_ElysiumPopulationFu:OnEnable()
+  self:RegisterEvent("CHAT_MSG_SYSTEM")
+
+  self:ScheduleRepeatingEvent("ScheduledWhoTimer", SendWhoQuery, 60)
+  SendWhoQuery()
+end
+
+function SendWhoQuery()
+  SetWhoToUI(0)
+  waitForWho = 1
+  SendWho("$-`")
+end
+
+function FuBar_ElysiumPopulationFu:CHAT_MSG_SYSTEM()
+  numResults, totalCount = GetNumWhoResults()
+  self:SetText(totalCount)
+end
+
+function FuBar_ElysiumPopulationFu:OnDisable()
+end
+
+function FuBar_ElysiumPopulationFu:OnClick()
+  self:SetText("Updating...")
+  SetWhoToUI(0)
+  waitForWho = 1
+  SendWho("$-`")
+end
---
+++ README.md
@@ -0,0 +1,2 @@
+# FuBar_ElysiumPopulationFu
+FuBar-2.0 Plugin for World of Warcraft 1.12.1 that shows the number of people online. Works only on Elysium WoW private servers.
